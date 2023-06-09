83b07e07c8c0bb874fade75d774f592699fcf9e3
dankeroni <theofpanagiotis@gmail.com>
2023-06-09 03:13:17 +0200

    implement encryption service

--- vanetza/security/encrypt_service.cpp
+++ vanetza/security/encrypt_service.cpp
@@ -1,17 +1,44 @@
-#include <vanetza/common/its_aid.hpp>
-#include <vanetza/common/runtime.hpp>
-#include <vanetza/security/backend.hpp>
-#include <vanetza/security/certificate_provider.hpp>
 #include <vanetza/security/sign_header_policy.hpp>
-#include <vanetza/security/sign_service.hpp>
-#include <cassert>
-#include <future>
+#include <vanetza/security/encrypt_service.hpp>
 
 namespace vanetza
 {
 namespace security
 {
 
+EncryptService straight_encrypt_serviceV3(BackendOpenSsl& backend)
+{
+    return [&backend](EncryptRequest&& request) {
+        const CertificateV3 &target_certificate = request.target_certificate;
+
+        auto public_key = target_certificate.get_public_key(backend);
+        auto curve_name = target_certificate.get_public_key_curve_name();
+        if (!public_key || !curve_name) {
+            throw std::runtime_error("Target certificate has no valid public key");
+        }
+
+        HashedId8 signer_info = target_certificate.calculate_hash();
+        ByteBuffer shared_info(signer_info.begin(), signer_info.end());
+
+        ByteBuffer data = convert_to_payload(request.plain_message);
+
+        EciesEncryptionResult encryption_res = backend.encrypt_data(*public_key, *curve_name, data, shared_info);
+
+        const std::array<uint8_t, 16> &aes_tag = encryption_res.aes_tag;
+        ByteBuffer merged_aes_data = encryption_res.aes_ciphertext;
+        merged_aes_data.insert(merged_aes_data.end(), aes_tag.begin(), aes_tag.end());
+
+        EncryptConfirm confirm { SecuredMessageV3(true) };
+        SecuredMessageV3 &secured_message = confirm.secured_message;
+        secured_message.set_aes_ccm_ciphertext(merged_aes_data, encryption_res.aes_nonce);
+        secured_message.add_cert_recip_info(
+            signer_info, *curve_name, encryption_res.ecies_ciphertext,
+            encryption_res.ecies_tag, encryption_res.ecies_pub_key);
+
+        // TODO: save session key
+        return confirm;
+    };
+}
 
 } // namespace security
 } // namespace vanetza
--- vanetza/security/encrypt_service.hpp
+++ vanetza/security/encrypt_service.hpp
@@ -2,12 +2,8 @@
 #define ENCRYPT_SERVICE_HPP_8CD77F6A
 
 #include <vanetza/common/byte_buffer.hpp>
-#include <vanetza/common/its_aid.hpp>
-#include <vanetza/common/position_provider.hpp>
-#include <vanetza/net/packet.hpp>
-#include <vanetza/security/int_x.hpp>
 #include <vanetza/security/secured_message.hpp>
-#include <vanetza/security/signer_info.hpp>
+#include <vanetza/security/backend_openssl.hpp>
 #include <functional>
 
 namespace vanetza
@@ -30,23 +26,22 @@ class DefaultSignHeaderPolicyV3;
 struct EncryptRequest
 {
     DownPacket plain_message;
-    ItsAid its_aid;
-    ByteBuffer permissions;
+    CertificateV3 target_certificate;
 };
 
 // mandatory SN-SIGN.confirm parameters
-struct SignConfirm
+struct EncryptConfirm
 {
-    SecuredMessageVariant secured_message;
+    SecuredMessageV3 secured_message;
 };
 
 
 /**
  * Equivalant of SN-ENCRYPT service in TS 102 723-8 v1.1.1
  */
-using EncryptService = std::function<SignConfirm(EncryptRequest&&)>;
+using EncryptService = std::function<EncryptConfirm(EncryptRequest&&)>;
 
-EncryptService straight_encrypt_serviceV3(CertificateProvider& certificate_provider, Backend& backend, SignHeaderPolicy& sign_header_policy);
+EncryptService straight_encrypt_serviceV3(BackendOpenSsl& backend);
 
 } // namespace security
 } // namespace vanetza
